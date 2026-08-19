055a8994dfe442a3ca28cad2612d6b8998ed70f1
Yared (Nur Solutions) <nursolutionsplc@gmail.com>
2026-08-19 16:40:37 +0300

    feat: _blank removed

--- script.js
+++ script.js
@@ -497,7 +497,7 @@ function downloadEmbeddedPDF(id, filename) {
   const a = document.createElement("a");
   a.href = url;
   a.download = filename;
-  a.target = "_blank";
+  // a.target = "_blank";
   a.rel = "noopener";
   document.body.appendChild(a);
   a.click();
